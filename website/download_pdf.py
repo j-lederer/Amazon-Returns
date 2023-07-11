f8fc3b86e61acfc1b38205eb4431ce0d699b17cb
j-lederer <125938397+j-lederer@users.noreply.github.com>
2023-07-11 23:34:19 +0000

    Download pdf preliminary2

--- website/download_pdf.py
+++ website/download_pdf.py
@@ -8,14 +8,23 @@ from .models import Tracking_ids
 from.database import load_queue_from_db
 
 # tracking_ids = Tracking_ids.query.filter_by(user_id=current_user.id).all()
-tracking_ids={}
+
 
 def download_queue_data(user_id):
     tracking_ids = load_queue_from_db(user_id)
-    PDF()
+    print("TRACKING_IDS_QUEUE")
+    print(tracking_ids)
+    pdf = PDF()
+    pdf.generate_pdf(tracking_ids)
 
 
 class PDF(FPDF):
+    def __init__(self):
+        super().__init__(format='A4')
+        self.alias_nb_pages()
+        self.add_page()
+        self.set_font('Arial', '', 12)
+      
     def header(self):
         # Logo
         # self.image('app/static/images/logo.jpg', 180, 8, 12)
@@ -36,7 +45,7 @@ class PDF(FPDF):
         # Page number
         self.cell(0, 10, 'Page ' + str(self.page_no()) + '/{nb}', 0, 0, 'C')
 
-    def table(self):
+    def table(self, tracking_ids):
         # Colors, line width and bold font
         self.set_font('Arial', 'B', 16)
         self.set_fill_color(255, 0, 0)
@@ -55,18 +64,14 @@ class PDF(FPDF):
         self.set_text_color(0)
         self.set_font('Arial', '', 8)
         for tracking_id in tracking_ids:
-            self.cell(25, 10, tracking_id.tracking, 1, 0, 'C')
-            self.cell(10, 10, str(tracking_id.SKU), 1, 0, 'C')
-            self.cell(75, 10, tracking_id.return_quantity, 1, 0, 'C')
+            self.cell(25, 10, tracking_id['tracking'], 1, 0, 'C')
+            self.cell(10, 10, str(tracking_id['SKU']), 1, 0, 'C')
+            self.cell(75, 10, tracking_id['return_quantity'], 1, 0, 'C')
             self.cell(40, 10, 'test', 1, 0, 'C')
             self.cell(45, 10, 'test@gmail.com', 1, 1, 'C')
         # Closing line
         self.cell(0, 10, '', 0, 1)
 
-    def __init__(self):
-        super().__init__(format='A4')
-        self.alias_nb_pages()
-        self.add_page()
-        self.set_font('Arial', '', 12)
-        self.table()
+    def generate_pdf(self, tracking_ids):
+        self.table(tracking_ids)
         self.output('website/static/files/queue.pdf', 'F')
